5ed2408a6c08810465223ace3c20d492ae101c63
Emem Attah <ememattah7@hotmail.co.uk>
2023-11-19 15:48:38 +0000

    Rethought structure of unit tests. Proceeding with one file as opposed to multiple. Have also coded first working unit test for the UserController.addUser method.

--- src/main/java/com/cbfacademy/apiassessment/services/UserService.java
+++ src/main/java/com/cbfacademy/apiassessment/services/UserService.java
@@ -5,7 +5,7 @@ import com.cbfacademy.apiassessment.entities.user.User;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Component
+@Service
 public class UserService {
 
     private static final String FILE_PATH= "src/main/resources/users.json";
--- src/test/java/com/cbfacademy/apiassessment/AppTests.java
+++ src/test/java/com/cbfacademy/apiassessment/AppTests.java
@@ -1,49 +1,54 @@
-//package com.cbfacademy.apiassessment;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.boot.test.web.server.LocalServerPort;
-//import org.springframework.context.annotation.Description;
-//import org.springframework.http.ResponseEntity;
-//
-//import java.net.URL;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//class AppTests {
-//
-//	@LocalServerPort
-//	private int port;
-//
-//	private URL base;
-//
-//	@Autowired
-//	private TestRestTemplate restTemplate;
-//
-//	@BeforeEach
-//	public void setUp() throws Exception {
-//		this.base = new URL("http://localhost:" + port + "/greeting");
-//	}
-//
-//	@Test
-//	@Description("/User endpoint returns expected response for User Id")
-//	public void user_ExpectedResponseWithID() {
-//		ResponseEntity<String> response = restTemplate.getForEntity(base.toString(), String.class);
-//
-//		assertEquals(200, response.getStatusCode().value());
-//		assertEquals("Hello World", response.getBody());
-//	}
-//
-//	@Test
-//	@Description("/greeting endpoint returns expected response for specified name parameter")
-//	public void greeting_ExpectedResponseWithNameParam() {
-//		ResponseEntity<String> response = restTemplate.getForEntity(base.toString() + "?name=John", String.class);
-//
-//		assertEquals(200, response.getStatusCode().value());
-//		assertEquals("Hello John", response.getBody());
-//	}
-//}
+package com.cbfacademy.apiassessment;
+
+import com.cbfacademy.apiassessment.controllers.UserController;
+import com.cbfacademy.apiassessment.entities.user.Role;
+import com.cbfacademy.apiassessment.entities.user.User;
+import com.cbfacademy.apiassessment.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AppTests {
+
+	@LocalServerPort
+	private int port;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserController controller;
+
+	private URL base;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+
+        controller = new UserController(userService);
+
+	}
+
+
+	@Test
+	@Description("/User endpoint returns expected response for addUser")
+	public void ExpectedResponseWithUserSuccessfullyAdded() {
+        User user = new User("1010101","Jennifer Lopez", Role.ReadOnly);
+		ResponseEntity<User> response = controller.addUser(user);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(user, response.getBody());
+	}
+}
